e0ffcd7b145042f9a2080aebf22d368e68f892ad
92euni <92_sudal@naver.com>
2022-06-10 03:26:26 +0900

    <람다식> 복습 1. @FunctionalInterface (함수형 인터페이스) 2. 하나의 메소드 필요

---
+++ JavaStudy_jungeun/src/j22_람다식/Math2.java
@@ -0,0 +1,11 @@
+package j22_람다식;
+@FunctionalInterface
+
+//람다를 쓸려면
+//1. @FunctionalInterface 달아야 하고 
+//2. 하나의 메소드가 필요하다
+
+public interface Math2 {
+	public double calc(double value1, double value2);
+//	public double calc(double value1, double value2);
+}
